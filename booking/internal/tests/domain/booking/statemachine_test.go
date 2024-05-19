acbf2d3d02e15b68400c0894492398c72b92b175
emannuel <emannuelloxt@hotmail.com>
2024-05-19 17:43:49 -0400

    test: should set status to refunded when refunding a booking with paid status

--- booking/internal/tests/domain/booking/statemachine_test.go
+++ booking/internal/tests/domain/booking/statemachine_test.go
@@ -12,6 +12,7 @@ func TestBookingStateMachine(t *testing.T) {
 	t.Run("should set status to paid when paying for a booking with created status", ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus)
 	t.Run("should set status to cancelled when cancelling a booking with created status", ShouldSetStatusToCancelledWhenCancellingABookingWithCreatedStatus)
 	t.Run("should set status to finished when finishing a booking with paid status", ShouldSetStatusToFinishedWhenFinishingABookingWithPaidStatus)
+	t.Run("should set status to refunded when refunding a booking with paid status", ShouldSetStatusToRefundedWhenRefundingABookingWithPaidStatus)
 }
 
 func ShouldAlwaysStartBookingWithCreatedStatus(t *testing.T) {
@@ -43,4 +44,13 @@ func ShouldSetStatusToFinishedWhenFinishingABookingWithPaidStatus(t *testing.T) 
 	booking.ChangeState(enums.Finish)
 
 	assert.Equal(t, enums.Finished, booking.CurrentStatus())
+}
+
+func ShouldSetStatusToRefundedWhenRefundingABookingWithPaidStatus(t *testing.T) {
+	booking := entities.NewBooking()
+
+	booking.ChangeState(enums.Pay)
+	booking.ChangeState(enums.Refund)
+
+	assert.Equal(t, enums.Refunded, booking.CurrentStatus())
 }
