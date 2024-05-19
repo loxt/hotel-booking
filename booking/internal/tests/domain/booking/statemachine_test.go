7a01d79e51edb4ad6721f69e3ba5ecb1b54869c1
emannuel <emannuelloxt@hotmail.com>
2024-05-19 17:41:18 -0400

    test: should set status to cancelled when cancelling a booking with created status

--- booking/internal/tests/domain/booking/statemachine_test.go
+++ booking/internal/tests/domain/booking/statemachine_test.go
@@ -10,6 +10,7 @@ import (
 func TestBookingStateMachine(t *testing.T) {
 	t.Run("should always start booking with created status", ShouldAlwaysStartBookingWithCreatedStatus)
 	t.Run("should set status to paid when paying for a booking with created status", ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus)
+	t.Run("should set status to cancelled when cancelling a booking with created status", ShouldSetStatusToCancelledWhenCancellingABookingWithCreatedStatus)
 }
 
 func ShouldAlwaysStartBookingWithCreatedStatus(t *testing.T) {
@@ -24,4 +25,12 @@ func ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus(t *testing.T) {
 	booking.ChangeState(enums.Pay)
 
 	assert.Equal(t, enums.Paid, booking.CurrentStatus())
+}
+
+func ShouldSetStatusToCancelledWhenCancellingABookingWithCreatedStatus(t *testing.T) {
+	booking := entities.NewBooking()
+
+	booking.ChangeState(enums.Cancel)
+
+	assert.Equal(t, enums.Cancelled, booking.CurrentStatus())
 }
