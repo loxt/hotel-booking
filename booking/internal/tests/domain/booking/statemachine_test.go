6fd7f2ce707c91605a8ab7ccaab8cf3de562dde0
emannuel <emannuelloxt@hotmail.com>
2024-05-19 17:42:12 -0400

    test: should set status to finished when finishing a booking with paid status

--- booking/internal/tests/domain/booking/statemachine_test.go
+++ booking/internal/tests/domain/booking/statemachine_test.go
@@ -11,6 +11,7 @@ func TestBookingStateMachine(t *testing.T) {
 	t.Run("should always start booking with created status", ShouldAlwaysStartBookingWithCreatedStatus)
 	t.Run("should set status to paid when paying for a booking with created status", ShouldSetStatusToPaidWhenPayingForABookingWithCreatedStatus)
 	t.Run("should set status to cancelled when cancelling a booking with created status", ShouldSetStatusToCancelledWhenCancellingABookingWithCreatedStatus)
+	t.Run("should set status to finished when finishing a booking with paid status", ShouldSetStatusToFinishedWhenFinishingABookingWithPaidStatus)
 }
 
 func ShouldAlwaysStartBookingWithCreatedStatus(t *testing.T) {
@@ -33,4 +34,13 @@ func ShouldSetStatusToCancelledWhenCancellingABookingWithCreatedStatus(t *testin
 	booking.ChangeState(enums.Cancel)
 
 	assert.Equal(t, enums.Cancelled, booking.CurrentStatus())
+}
+
+func ShouldSetStatusToFinishedWhenFinishingABookingWithPaidStatus(t *testing.T) {
+	booking := entities.NewBooking()
+
+	booking.ChangeState(enums.Pay)
+	booking.ChangeState(enums.Finish)
+
+	assert.Equal(t, enums.Finished, booking.CurrentStatus())
 }
